67d3bd9657953addc196d437783babc687441b29
Wanyu Jiang <wanyu19930930@gmail.com>
2025-05-16 20:53:34 +0900

    Add view for Home

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to 'pages#home'
+  root to: 'pages#home'
   get '/about', to: 'pages#about'
 end
